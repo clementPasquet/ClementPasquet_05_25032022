3c3c718e83d5e9bb1592b8f5d64ab6ccf89a974b
Clement Pasquet <clemsp32@hotmail.fr>
2022-03-27 18:37:29 +0200

    fetch

--- front/js/accueil.js
+++ front/js/accueil.js
@@ -3,10 +3,14 @@ var url = "http://localhost:3000/api/products";
 fetch(url)
 .then(data => data.json())
 .then(jsonListArticle => {
-    for (let jsonArticle of jsonArticle){
+    for (let jsonArticle of jsonListArticle){
         let article = new Article (jsonArticle);
-        document.querySelector(article).innerHtml +=  <img src="${article.imageUrl}" alt="Lorem ipsum dolor sit amet, Kanap name1">
-                                                     <h3 class="productName">${article.name]</h3>
-                                                    <p class="productDescription">${article.description}</p>
-    }
-})
+        console.log(article);
+        document.querySelector('#items').innerHtml +=                ` <a id="productLink" href="./product.html?id=42">
+                                                                          <article>
+                                                                 <img src="${article.imageUrl}" alt="Lorem ipsum dolor sit amet, Kanap name1">
+                                                                 <h3 class="productName">${article.name}</h3>
+                                                                          <p class="productDescription">${article.description}</p>
+                                                                        </article>
+                                                                              </a> `   
+}}); 
